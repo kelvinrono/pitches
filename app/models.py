3dd9493ef189569a0276de1e0d8f88bf2b87cea0
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-06-12 19:41:34 +0300

    add an email column

--- app/models.py
+++ app/models.py
@@ -10,9 +10,10 @@ def load_user(user_id):
 
 class User(db.Model):
     __tablename__ ='users'
+
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(255))
-    email=db.Column(db.String(255))
+    email=db.Column(db.String(255), unique=True, index = True)
     password_hash=db.Column(db.String(255))
     pass_secure = db.Column(db.String(255), unique=True, index=True)
 
