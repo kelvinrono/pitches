c124e1140dfecf9f8ab0d83fe8cf8ee0c0349c5b
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-06-13 01:01:05 +0300

    import the flask uploads and initialize

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 login_manager=LoginManager()
 login_manager.session_protectio='strong'
@@ -11,6 +12,7 @@ login_manager.login_view='auth.login'
 
 db=SQLAlchemy()
 bootstrap=Bootstrap()
+photos=UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app=Flask(__name__)
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY =os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:kelvin@localhost/pitches'
+    UPLOAD_PHOTOS_DEST = 'app/static/photos'
 
 class ProdConfig(Config):
     pass
