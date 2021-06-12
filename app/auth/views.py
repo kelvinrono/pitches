49c7591b6733dd1397372c71aeb4677cb830da6f
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-06-12 23:09:36 +0300

    add a login route

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,3 +21,16 @@ def register():
         title = "Account created"
     return render_template('auth/register.html',registration_form = form)
 
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "watchlist login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
