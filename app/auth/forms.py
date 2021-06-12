f8e4dbd1bab4d6b7634676ddd94270528946f764
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-06-12 23:01:54 +0300

    make the imports required for the  login setup

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email,EqualTo, ValidationError
 from ..models import User
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
