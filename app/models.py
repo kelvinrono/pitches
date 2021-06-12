beb32ece60f11a4750c9e78a97c28ff791f39ba1
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-06-13 00:20:12 +0300

    allow addition of user profile

--- app/models.py
+++ app/models.py
@@ -8,12 +8,14 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ ='users'
 
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(255))
     email=db.Column(db.String(255), unique=True, index = True)
+    bio=db.Column(db.String(255))
+    profile_pic_path=db.Column(db.String())
     password_hash=db.Column(db.String(255))
     pass_secure = db.Column(db.String(255), unique=True, index=True)
 
